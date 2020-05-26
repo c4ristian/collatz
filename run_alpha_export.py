7a08a70138a5a51f2be6b605c9f534e303cf4a2b
c4ristian <christian.koch@gmx.li>
2020-05-26 11:41:54 +0200

    Variable names of export jobs and notebooks standardised

--- run_alpha_export.py
+++ run_alpha_export.py
@@ -15,7 +15,7 @@ from collatz import commons
 if __name__ == '__main__':
     K_FACTORS = [1, 3, 5, 7, 9]
     MAX_START_VALUE = 3999
-    V1_RANGE = range(1, MAX_START_VALUE + 1, 2)
+    V_1_RANGE = range(1, MAX_START_VALUE + 1, 2)
     MAX_ITERATIONS = 100
     N = ((MAX_START_VALUE + 1) / 2) * len(K_FACTORS)
     FILE_NAME = "./data/alpha_sequences.csv"
@@ -29,43 +29,46 @@ if __name__ == '__main__':
     for k in K_FACTORS:
         logging.info("Generating sequences for k=%d", k)
 
-        for v1 in V1_RANGE:
+        for v_1 in V_1_RANGE:
             # Create the sequence
             SEQUENCE_ID = SEQUENCE_ID + 1
-            odds = commons.odd_collatz_sequence(v1, k, MAX_ITERATIONS)
+            odds = commons.odd_collatz_sequence(v_1, k, MAX_ITERATIONS)
             next_odds = odds[1:]
             odds.pop()
 
-            current_frame = pd.DataFrame({"vi": odds})
-            current_frame["vi_1"] = next_odds
-            current_frame["kvi+1"] = current_frame["vi"].apply(
-                commons.next_collatz_number, args=(k,))
+            current_frame = pd.DataFrame({"v_i": odds})
             current_frame["sequence_id"] = SEQUENCE_ID
             current_frame["sequence_len"] = len(current_frame)
-            current_frame["v1"] = v1
             current_frame["n"] = current_frame.index + 1
             current_frame["k_factor"] = k
+            current_frame["v_1"] = v_1
+            current_frame["kv_i+1"] = current_frame["v_i"].apply(
+                commons.next_collatz_number, args=(k,))
+            current_frame["v_i+"] = next_odds
+            current_frame["terminal"] = \
+                (current_frame["v_i+"] == current_frame["v_1"]) | \
+                (current_frame["v_i+"] == 1)
 
-            current_frame["alpha_i"] = current_frame["kvi+1"].apply(commons.trailing_zeros)
+            current_frame["alpha_i"] = current_frame["kv_i+1"].apply(commons.trailing_zeros)
             current_frame["alpha_i"] = current_frame["alpha_i"].astype('int64')
-            current_frame["alpha_i_max"] = log2(k) + current_frame["vi"].apply(log2)
-            current_frame["alpha_i_max"] += (1 + 1/(k * current_frame["vi"])).apply(log2)
+            current_frame["alpha_i_max"] = log2(k) + current_frame["v_i"].apply(log2)
+            current_frame["alpha_i_max"] += (1 + 1/(k * current_frame["v_i"])).apply(log2)
             # Round result here to avoid loss of precision errors
             current_frame["alpha_i_max"] = current_frame["alpha_i_max"].round(9)
-            current_frame["alpha_sum"] = current_frame["alpha_i"].cumsum()
-            current_frame["alpha_pred"] = (log2(k) * current_frame["n"]).astype('int64') + 1
-            current_frame["alpha_max"] = log2(v1) + (current_frame["n"] * log2(k))
+            current_frame["alpha"] = current_frame["alpha_i"].cumsum()
+            current_frame["alpha_cycle"] = (log2(k) * current_frame["n"]).astype('int64') + 1
+            current_frame["alpha_max"] = log2(v_1) + (current_frame["n"] * log2(k))
             current_frame["alpha_max"] = current_frame["alpha_max"].astype('int64') + 1
 
             print_frame = current_frame[[
-                "sequence_id", "sequence_len", "n", "k_factor", "v1",
-                "vi", "vi_1", "alpha_i", "alpha_i_max", "alpha_sum",
-                "alpha_pred", "alpha_max"]]
+                "sequence_id", "sequence_len", "n", "k_factor", "v_1",
+                "v_i", "v_i+", "terminal", "alpha_i", "alpha_i_max", "alpha",
+                "alpha_cycle", "alpha_max"]]
 
             print_frame.columns = [
-                "sequence_id", "sequence_len", "n", "k", "v1",
-                "vi", "vi_1", "a_i", "a_i_max", "a_sum",
-                "a_pred", "a_max"]
+                "sequence_id", "sequence_len", "n", "k", "v_1",
+                "v_i", "v_i+", "terminal", "a_i", "a_i_max", "a",
+                "a_cycle", "a_max"]
 
             if OUTPUT_FRAME is not None:
                 OUTPUT_FRAME = OUTPUT_FRAME.append(print_frame)
--- run_binary_export.py
+++ run_binary_export.py
@@ -13,7 +13,7 @@ from collatz import commons
 if __name__ == '__main__':
     K_FACTORS = [1, 3, 5, 7, 9]
     MAX_START_VALUE = 3999
-    V1_RANGE = range(1, MAX_START_VALUE + 1, 2)
+    V_1_RANGE = range(1, MAX_START_VALUE + 1, 2)
     MAX_ITERATIONS = 100
     N = ((MAX_START_VALUE + 1) / 2) * len(K_FACTORS)
     FILE_NAME = "./data/binary_sequences.csv"
@@ -27,34 +27,38 @@ if __name__ == '__main__':
     for k in K_FACTORS:
         logging.info("Generating sequences for k=%d", k)
 
-        for v1 in V1_RANGE:
+        for v_1 in V_1_RANGE:
             # Create the sequence
             SEQUENCE_ID = SEQUENCE_ID + 1
-            odds = commons.odd_collatz_sequence(v1, k, MAX_ITERATIONS)
+            odds = commons.odd_collatz_sequence(v_1, k, MAX_ITERATIONS)
             next_odds = odds[1:]
             odds.pop()
 
             # Derive new fields
-            current_frame = pd.DataFrame({"vi": odds})
-            current_frame["kvi+1"] = current_frame["vi"].apply(
+            current_frame = pd.DataFrame({"v_i": odds})
+            current_frame["kv_i+1"] = current_frame["v_i"].apply(
                 commons.next_collatz_number, args=(k,))
-            current_frame["vi_1"] = next_odds
+            current_frame["v_i+"] = next_odds
             current_frame["sequence_id"] = SEQUENCE_ID
             current_frame["sequence_len"] = len(current_frame)
-            current_frame["v1"] = v1
+            current_frame["v_1"] = v_1
             current_frame["n"] = current_frame.index + 1
             current_frame["k_factor"] = k
 
+            current_frame["terminal"] = \
+                (current_frame["v_i+"] == current_frame["v_1"]) | \
+                (current_frame["v_i+"] == 1)
+
             current_frame["bin_len"] = \
-                current_frame["vi"].apply(log2).astype('int64') + 1
+                current_frame["v_i"].apply(log2).astype('int64') + 1
 
             current_frame["next_bin_len"] = \
-                current_frame["kvi+1"].apply(log2).astype('int64') + 1
+                current_frame["kv_i+1"].apply(log2).astype('int64') + 1
 
             current_frame["bin_diff"] = current_frame["next_bin_len"] - current_frame["bin_len"]
             current_frame["lambda_i"] = current_frame["bin_diff"]
             current_frame.loc[current_frame["lambda_i"] < 0, "lambda_i"] = 0
-            current_frame["lambda_sum"] = current_frame["lambda_i"].cumsum()
+            current_frame["lambda"] = current_frame["lambda_i"].cumsum()
 
             current_frame["lambda_i_min"] = int(log2(k))
             current_frame["lambda_i_max"] = int(log2(k) + 1)
@@ -63,35 +67,35 @@ if __name__ == '__main__':
             current_frame["lambda_min"] = current_frame["lambda_hyp"].astype('int64')
             current_frame["lambda_max"] = current_frame["lambda_hyp"].astype('int64') + 2
 
-            current_frame["alpha_i"] = current_frame["kvi+1"].apply(commons.trailing_zeros)
+            current_frame["alpha_i"] = current_frame["kv_i+1"].apply(commons.trailing_zeros)
             current_frame["alpha_i"] = current_frame["alpha_i"].astype('int64')
-            current_frame["alpha_sum"] = current_frame["alpha_i"].cumsum()
-            current_frame["alpha_pred"] = (log2(k) * current_frame["n"]).astype('int64') + 1
-            current_frame["alpha_max"] = log2(v1) + (current_frame["n"] * log2(k))
+            current_frame["alpha"] = current_frame["alpha_i"].cumsum()
+            current_frame["alpha_cycle"] = (log2(k) * current_frame["n"]).astype('int64') + 1
+            current_frame["alpha_max"] = log2(v_1) + (current_frame["n"] * log2(k))
             current_frame["alpha_max"] = current_frame["alpha_max"].astype('int64') + 1
 
             current_frame["omega_i"] = current_frame["lambda_i"] - current_frame["alpha_i"]
-            current_frame["omega_sum"] = current_frame["lambda_sum"] - current_frame["alpha_sum"]
+            current_frame["omega"] = current_frame["lambda"] - current_frame["alpha"]
 
             current_frame["omega_i_max"] = current_frame["lambda_i_max"] - 1
             current_frame["omega_max"] = current_frame["lambda_max"] - current_frame["n"]
 
             print_frame = current_frame[[
-                "sequence_id", "sequence_len", "n", "k_factor", "v1",
-                "vi", "kvi+1", "vi_1", "alpha_i", "alpha_sum",
-                "alpha_pred", "alpha_max", "bin_len", "next_bin_len",
+                "sequence_id", "sequence_len", "n", "k_factor", "v_1",
+                "v_i", "kv_i+1", "v_i+", "alpha_i", "alpha",
+                "alpha_cycle", "alpha_max", "bin_len", "next_bin_len",
                 "lambda_i", "lambda_i_min", "lambda_i_max",
-                "lambda_sum", "lambda_min", "lambda_max",
-                "omega_i", "omega_i_max", "omega_sum", "omega_max"
+                "lambda", "lambda_min", "lambda_max",
+                "omega_i", "omega_i_max", "omega", "omega_max"
             ]]
 
             print_frame.columns = [
-                "sequence_id", "sequence_len", "n", "k", "v1",
-                "vi", "kvi+1", "vi_1", "a_i", "a_sum",
-                "a_pred", "a_max", "bin_len", "next_bin_len",
+                "sequence_id", "sequence_len", "n", "k", "v_1",
+                "v_i", "kv_i+1", "v_i+", "a_i", "a",
+                "a_cycle", "a_max", "bin_len", "next_bin_len",
                 "l_i", "l_i_min", "l_i_max",
-                "l_sum", "l_min", "l_max",
-                "o_i", "o_i_max", "o_sum", "o_max"
+                "l", "l_min", "l_max",
+                "o_i", "o_i_max", "o", "o_max"
             ]
 
             if OUTPUT_FRAME is not None:
