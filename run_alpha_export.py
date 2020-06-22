27498f4c1715d99f1f762dfaf27223bbbde2a23b
c4ristian <christian.koch@gmx.li>
2020-06-22 18:03:36 +0200

    Documentation enhanced

--- run_alpha_export.py
+++ run_alpha_export.py
@@ -1,5 +1,5 @@
 """
-This program exports data on the alphas of collatz sequences
+This program exports data on the alphas of collatz sequences and related features
 into a csv file. Only odd Collatz numbers are included. The sample is used
 to validate several mathematical theorems.
 """
