47f6aeef5339fc915aba37ce8643d12d3523d06e
c4ristian <christian.koch@gmx.li>
2020-07-07 15:44:50 +0200

    Documentation enhanced

--- notebooks/nbsetup.py
+++ notebooks/nbsetup.py
@@ -1,7 +1,7 @@
 """
-This python module executes different functions to set up the notebooks of
-this library. The module solves for example a problem with the loading of
-modules that occurs in some jupyter environments.
+This module provides functions for setting up the notebooks of
+this library. For example, it solves a problem loading
+other modules that occurs in some jupyter environments.
 """
 
 # Imports
@@ -15,7 +15,7 @@ sys.path.append("..")
 
 def set_default_pd_options():
     """
-    This functions sets different default options for pandas.
+    This functions sets default options for pandas.
     :return: None.
     """
     pd.set_option('display.expand_frame_repr', False)
