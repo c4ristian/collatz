efc568961ab860cb5357e2428d6c66c84dcc820b
c4ristian <christian.koch@gmx.li>
2020-10-03 15:48:19 +0200

    Documentation enhanced

--- collatz/graph.py
+++ collatz/graph.py
@@ -1,5 +1,5 @@
 """
-This module provides methods to create and analyse collatz graphs.
+This module provides methods to create and analyse Collatz graphs.
 """
 
 import pandas as pd
@@ -7,7 +7,7 @@ import pandas as pd
 
 def get_odd_predecessor(odd_int, index, k=3):
     """
-    This method calculates the odd predecessor for a certain odd number in a collatz graph.
+    This method calculates the odd predecessor for a certain odd number in a Collatz graph.
     For every odd number there are n predecessors. The variable index [0..n] specifies which
     predecessor is returned. The method is based on a deterministic algorithm.
     It currently works only for the k-factors (1,3,5,7,9).
@@ -51,24 +51,24 @@ def get_odd_predecessor(odd_int, index, k=3):
         if power:
             result = (odd_int * 2 ** (power + (6 * index)) - 1) // k
     else:
-        raise TypeError("Parameter k not in (1,3,5,7, 9)")
+        raise TypeError("Parameter k not in (1,3,5,7,9)")
 
     return result
 
 
 def create_collatz_graph(start_value, k=3, predecessor_count=3, iteration_count=3):
     """
-    This method creates a collatz graph, consisting of odd numbers, beginning with a
-    certain odd integer value as root node. The method determines the predecessors
-    of the root with the function get_odd_predecessor.
+    This method creates a Collatz graph consisting of odd numbers, starting with a
+    certain odd integer as root node. The method determines the predecessors
+    of the root node using the function get_odd_predecessor.
 
     The function is optimised for handling arbitrary big integers.
 
-    :param start_value: Odd integer root node to start the graph with.
+    :param start_value: Odd integer as root node.
     :param k: The factor odd numbers are multiplied with in the sequence (default 3).
+    :param predecessor_count: The number of predecessors to determine for every node.
     :param iteration_count: The number of iterations to perform. This parameter determines
     the depth of the tree.
-    :param predecessor_count: The number of predecessors to determine for every odd number.
     :return: The collatz graph as data frame.
     """
     result_frame = pd.DataFrame({
@@ -104,17 +104,17 @@ def create_collatz_graph(start_value, k=3, predecessor_count=3, iteration_count=
 
 def create_reverse_graph(start_value, k=3, successor_count=3, iteration_count=3):
     """
-    This method creates an inverse collatz graph, consisting of odd numbers, beginning with a
-    certain odd integer value as root node. The method internally builds on the function
-    create_collatz_graph and swaps the successors / predecessors.
+    This method creates a reverse Collatz graph consisting of odd numbers, starting with a
+    certain odd integer as root node. The method internally builds on the function
+    create_collatz_graph and swaps the successor and predecessor columns.
 
     The function is optimised for handling arbitrary big integers.
 
-    :param start_value: Odd integer root node to start the graph with.
+    :param start_value: Odd integer as root node.
     :param k: The factor odd numbers are multiplied with in the sequence (default 3).
+    :param successor_count: The number of successors to determine for every node.
     :param iteration_count: The number of iterations to perform. This parameter determines
     the depth of the tree.
-    :param successor_count: The number of successor to determine for every odd number.
     :return: The collatz graph as data frame.
     """
     graph_frame = create_collatz_graph(start_value, k, successor_count, iteration_count)
